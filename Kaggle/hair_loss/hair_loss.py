201eeb08bd083dc7b0a98cac3b81c7ead2623546
HugoRICHAR <hugo.richardh@gmail.com>
2023-12-01 19:01:46 +0100

    making changes to dev

--- Kaggle/hair_loss/hair_loss.py
+++ Kaggle/hair_loss/hair_loss.py
@@ -3,5 +3,7 @@ import pandas as pd
 import numpy as np
 
 # load the data
-hair_loss=pd.read_csv('/Users/hugz/Projects/Kaggle/hair_loss.csv')
+hair_loss=pd.read_csv('Kaggle/hair_loss/hair_loss.csv')
+
+
 
