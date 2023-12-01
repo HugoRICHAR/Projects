1318f7541914928576e680e94b960933b9fe6eb3
HugoRICHAR <hugo.richardh@gmail.com>
2023-12-01 19:33:44 +0100

    making changes to dev

--- Kaggle/hair_loss/hair_loss.py
+++ Kaggle/hair_loss/hair_loss.py
@@ -1,9 +1,21 @@
+# https://www.kaggle.com/datasets/brijlaldhankour/hair-loss-dataset/data
+
 # import the packages
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 # load the data
-hair_loss=pd.read_csv('Kaggle/hair_loss/hair_loss.csv')
+hair_loss = pd.read_csv('Kaggle/hair_loss/hair_loss.csv')
 
+#explore the data
+hair_loss.head()
+hair_loss.shape
+
+#check for missing values
+hair_loss.isna().sum()
+
+#look at some basic stats
+hair_loss.describe()
 
 
