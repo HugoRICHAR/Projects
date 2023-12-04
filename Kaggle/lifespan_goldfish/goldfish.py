699e7c5105274591b47eccd5b2b94d6ecc005283
HugoRICHAR <hugo.richardh@gmail.com>
2023-12-04 22:51:34 +0100

    Creation of the new project, goldfish lifespan

--- Kaggle/lifespan_goldfish/goldfish.py
+++ Kaggle/lifespan_goldfish/goldfish.py
@@ -9,6 +9,24 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import OneHotEncoder
+
 
 # load the data
 fish_data = pd.read_csv('Kaggle/lifespan_goldfish/fish_data.csv')
+
+#remove the useless data
+fish_data.drop('id',axis='columns',inplace=True)
+
+#look at the missing data
+fish_data.isnull().sum()
+
+#one hot encoding
+habitat=pd.get_dummies(fish_data['habitat'])
+fish_data=fish_data.join(habitat)
+color=pd.get_dummies(fish_data['color'])
+fish_data=fish_data.join(color)
+gender=pd.get_dummies(fish_data['Gender'])
+fish_data=fish_data.join(gender)
+
+
